3c3db113c833c8a9b1d9a4d9f70f2a600d5402d8
Paul Joseph Robin <48252793+p-j-r@users.noreply.github.com>
2019-12-17 19:44:26 +0530

    Create qr_generator.py
    A program to generate random QR codes of college IDs.

---
+++ Test/qr_generator.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 17:53:43 2019
+
+@author: paulj
+"""
+import os
+import qrcode
+from random import randint,choice
+
+course=['bphy','bche','phil','sans','econ','bapa','bscp']
+count=1
+while(count<=1200):
+    yy=str(randint(17,19))
+    cr=choice(course)
+    no=str(randint(1,60))
+    if (len(no)==1):
+        no="0"+no
+    data =yy+cr+"0"+no          # eg: 19BPHY060
+    
+    filename = data+".png"       
+    
+    path="C:\\Users\alpha\Documents\QR"
+    fullname=os.path.join(path,filename)
+    img = qrcode.make(data)
+    img.save(fullname)
+    
+    count=count+1
+    
+#print(count)
+# This program generates about 780 images. I guess it is because id is getting repeated 
+# And Windows is preventing same filenames from getting saved or it's being replaced.
